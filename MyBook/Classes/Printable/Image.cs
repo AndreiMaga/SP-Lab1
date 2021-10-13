d9af90025bdc1c9d7d6c00a1552953e89f8eca4c
Andrei Maga <andreimariomaga@gmail.com>
2021-10-13 13:02:59 +0300

    Try to add an image linking to an actual  PNG image and see what happens :smile:

--- MyBook/Classes/Printable/Image.cs
+++ MyBook/Classes/Printable/Image.cs
@@ -1,18 +1,83 @@
 ﻿using MyBook.Interfaces;
 using System;
+using System.Drawing;
+using System.IO;
+using System.Text;
 
 namespace MyBook.Classes.Printable
 {
     class Image : IPrintable
     {
-        string mImage;
-        public Image(string image)
+        readonly string mImage;
+
+		private const int mWidth = 80;
+
+		private readonly string[] mSupportedChars = { "#", "#", "@", "%", "=", "+", "*", ":", "-", ".", " " };
+
+		public Image(string image)
         {
             mImage = image;
         }
         public void Print()
         {
-            Console.WriteLine($"{mImage}");
+			Console.WriteLine($"{mImage}");
+
+			if (File.Exists(mImage))
+			{
+				PrintImage();
+				return;
+			}
         }
-    }
+
+        private void PrintImage()
+        {
+			Console.WriteLine(ConvertToAscii(GetReSizedImage((Bitmap)Bitmap.FromFile(mImage), mWidth)));
+		}
+
+		private Bitmap GetReSizedImage(Bitmap inputBitmap, int asciiWidth)
+		{
+			int asciiHeight = (int)Math.Ceiling((double)inputBitmap.Height * asciiWidth / inputBitmap.Width);
+			Bitmap result = new Bitmap(asciiWidth, asciiHeight);
+			Graphics g = Graphics.FromImage(result);
+			g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+			g.DrawImage(inputBitmap, 0, 0, asciiWidth, asciiHeight);
+			g.Dispose();
+			return result;
+		}
+
+		private string ConvertToAscii(Bitmap image)
+		{
+            bool toggle = false;
+			StringBuilder sb = new StringBuilder();
+			for (int h = 0; h < image.Height; h++)
+			{
+				for (int w = 0; w < image.Width; w++)
+				{
+					Color pixelColor = image.GetPixel(w, h);
+					int red = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
+					int green = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
+					int blue = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
+					Color grayColor = Color.FromArgb(red, green, blue);
+					if (!toggle)
+					{
+						int index = (grayColor.R * 10) / 255;
+						sb.Append(mSupportedChars[index]);
+					}
+				}
+
+				if (!toggle)
+				{
+					sb.Append(Environment.NewLine);
+					toggle = true;
+				}
+				else
+				{
+					toggle = false;
+				}
+			}
+
+			return sb.ToString();
+		}
+
+	}
 }

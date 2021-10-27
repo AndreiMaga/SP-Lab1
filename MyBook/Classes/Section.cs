15c95caf50e3471f3da22d3edb2c1b66da3f5c51
Andrei Maga <andreimariomaga@gmail.com>
2021-10-27 13:01:48 +0300

    Added TableOfContents

--- MyBook/Classes/Section.cs
+++ MyBook/Classes/Section.cs
@@ -10,51 +10,42 @@ namespace MyBook.Classes
 
         public bool ShouldPrintTitle { get; set; } = true;
 
-        readonly List<IPrintable> mPrintables;
+        public readonly List<IPrintable> Printables;
 
         public Section(string title)
         {
             Title = title;
-            mPrintables = new List<IPrintable>();
-        }
-
-        public int CreateSubChapter(string title)
-        {
-            Section chapter = new Section(title);
-
-            mPrintables.Add(chapter);
-
-            return mPrintables.IndexOf(chapter);
+            Printables = new List<IPrintable>();
         }
 
         public virtual void Print()
         {
             if(ShouldPrintTitle)
                 Console.WriteLine($"{Title}");
-            mPrintables.ForEach(chapter => chapter.Print());
+            Printables.ForEach(chapter => chapter.Print());
         }
 
         public IPrintable Add(IPrintable printable)
         {
             IPrintable cloned = (IPrintable)printable.Clone();
-            mPrintables.Add(cloned);
+            Printables.Add(cloned);
             return cloned;
         }
 
         public void Remove(IPrintable printable)
         {
-            mPrintables.Remove(printable);
+            Printables.Remove(printable);
         }
 
         public IPrintable Get(int index)
         {
-            return mPrintables[index];
+            return Printables[index];
         }
 
         public virtual object Clone()
         {
             Section s = new Section(Title);
-            mPrintables.ForEach(b => s.Add(b));
+            Printables.ForEach(b => s.Add(b));
             return s;
         }
     }

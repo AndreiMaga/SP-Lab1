5bc01a5e79acdff484b99634737eefaf35296f5c
Andrei Maga <andreimariomaga@gmail.com>
2021-11-24 11:20:48 +0200

    Create DocumentManager.cs

---
+++ MyBook/Classes/DocumentManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBook.Classes
+{
+    class DocumentManager
+    {
+
+        public Book Book { get; set; }
+
+        private static DocumentManager _instance;
+
+        private DocumentManager() { }
+
+        public static DocumentManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new DocumentManager();
+
+                return _instance;
+            }
+        }
+    }
+}
